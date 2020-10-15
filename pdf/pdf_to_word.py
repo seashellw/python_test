a8df0e284bbe7d2bf43d2c7c58436656ed41c45a
seashellw <15839366985@163.com>
2020-10-15 09:39:06 +0800

    pdf文件处理修改

--- pdf/pdf_to_word.py
+++ pdf/pdf_to_word.py
@@ -10,36 +10,42 @@ zoom_x = 1.0
 zoom_y = 1.0
 trans = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
 # 原pdf文件全名
-open_file_path = r'C:\Users\用户名\Desktop\vuejs.pdf'
+open_file_path = r'C:\Users\15839\Desktop\原创力 C#网络应用编程 第3版_13619182_compressed.pdf'
 # 图片存储目录
-save_file_path = r'C:\Users\用户名\Desktop\新建文件夹'
+save_file_path = r'C:\Users\15839\Desktop\新建文件夹'
 pdf = fitz.open(open_file_path)
 # 在内存中创建一个新docx文档对象
 doc = docx.Document()
 
+docx_num=4
+
 for i in tqdm(range(pdf.pageCount)):
-	if i < pdf.pageCount / 2:
-		pm = pdf[i].getPixmap(matrix=trans, alpha=False)
-		png_path = save_file_path + '/%s.png' % i
-		# 保存提取出的图片
-		pm.writePNG(png_path)
-		# 将图片插入docx文档的尾部,并设置宽度
-		doc.add_picture(png_path, width=shared.Cm(15))
 
-	elif i == pdf.pageCount / 2:
-		pm = pdf[i].getPixmap(matrix=trans, alpha=False)
-		png_path = save_file_path + '/%s.png' % i
-		pm.writePNG(png_path)
-		doc.add_picture(png_path, width=shared.Cm(15))
-		# 保存docx文件,参数为文件全名
-		doc.save(r'C:\Users\用户名\Desktop\test1.docx')
-		doc = docx.Document()
 
-	else:
-		pm = pdf[i].getPixmap(matrix=trans, alpha=False)
-		png_path = save_file_path + '/%s.png' % i
-		pm.writePNG(png_path)
-		doc.add_picture(png_path, width=shared.Cm(15))
+
+
+    if i < pdf.pageCount / 2:
+        pm = pdf[i].getPixmap(matrix=trans, alpha=False)
+        png_path = save_file_path + '/%s.png' % i
+        # 保存提取出的图片
+        pm.writePNG(png_path)
+        # 将图片插入docx文档的尾部,并设置宽度
+        doc.add_picture(png_path, width=shared.Cm(15))
+
+    elif i == pdf.pageCount / 2:
+        pm = pdf[i].getPixmap(matrix=trans, alpha=False)
+        png_path = save_file_path + '/%s.png' % i
+        pm.writePNG(png_path)
+        doc.add_picture(png_path, width=shared.Cm(15))
+        # 保存docx文件,参数为文件全名
+        doc.save(r'C:\Users\15839\Desktop\test1.docx')
+        doc = docx.Document()
+
+    else:
+        pm = pdf[i].getPixmap(matrix=trans, alpha=False)
+        png_path = save_file_path + '/%s.png' % i
+        pm.writePNG(png_path)
+        doc.add_picture(png_path, width=shared.Cm(15))
 
 # 保存docx文件,参数为文件全名
-doc.save(r'C:\Users\用户名\Desktop\test2.docx')
+doc.save(r'C:\Users\15839\Desktop\test2.docx')
