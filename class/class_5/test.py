a6b69dd6de8da9169c0657f3e357c76589fdd914
seashellw <15839366985@163.com>
2020-10-21 22:22:01 +0800

    f

--- class/class_5/test.py
+++ class/class_5/test.py
@@ -56,34 +56,45 @@
 
 
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 
-class Solution:
-    def reorderList(self, head: ListNode) -> None:
-        """
-        Do not return anything, modify head in-place instead.
-        """
-        if not head:
-            return
-        # 找到中点
-        slow, fast = head, head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        # 翻转后半
-        pre, cur = None, slow.next
-        slow.next = None
-        while cur:
-            nxt = cur.next
-            cur.next = pre
-            pre, cur = cur, nxt
-        # 交替合并
-        p, q = head, pre
-        while q:
-            nxt = p.next
-            p.next = q
-            p, q = q, nxt
+# class Solution:
+#     def reorderList(self, head: ListNode) -> None:
+#         """
+#         Do not return anything, modify head in-place instead.
+#         """
+#         if not head:
+#             return
+#         # 找到中点
+#         slow, fast = head, head
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         # 翻转后半
+#         pre, cur = None, slow.next
+#         slow.next = None
+#         while cur:
+#             nxt = cur.next
+#             cur.next = pre
+#             pre, cur = cur, nxt
+#         # 交替合并
+#         p, q = head, pre
+#         while q:
+#             nxt = p.next
+#             p.next = q
+#             p, q = q, nxt
+
+def info2(name, *args, country='美国'):  # 这种不定长的一定要放最后
+    print('姓名', name, ',国家', country, ",其他", args)
+
+# info2('llc',19,'ok')#顺序参数  如果没有country=‘美国’这个默认参数，会把19赋值给 国家
+# 运行结果  姓名 llc ,国家 19 ,其他 ('ok',)
+
+
+# info2('llc',19,'okk','美国')#那个*args不定长。比如此例，llc是位置参数，传给第一个形参name。第二个19顺序传给了country，剩下的俩都给args了
+# 姓名 llc ,国家 19 ,其他 ('okk', '美国')
+info2('llc', 19, 'okk', country="中国")
